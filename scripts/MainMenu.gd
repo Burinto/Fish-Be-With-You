222db11be844b4c1a24fabfbaa9e4da6f0badc06
Burinto <brentolym@gmail.com>
2025-06-02 01:09:57 +0800

    commit

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,17 +1,20 @@
+#MainMenu.gd
 extends Node2D
 
 var nextScene = Global.load_player_progress()
-
 
 func _ready():
 	pass # Replace with function body.
 
 func _on_NewGame_pressed():
-	get_tree().change_scene("res://src/levels/Level1.tscn")
-	print("New Game pressed: going to tutorial") #new game dapat pupunta ng tutorial?
-
+	var loading_scene = preload("res://src/scenes/LoadingScreen.tscn").instance()
+	loading_scene.target_scene_path = "res://src/levels/Level1.tscn"
+	get_tree().root.add_child(loading_scene)
+	
 func _on_LoadGame_pressed():
-	get_tree().change_scene(nextScene)
+	var loading_scene = preload("res://src/scenes/LoadingScreen.tscn").instance()
+	loading_scene.target_scene_path = nextScene
+	get_tree().root.add_child(loading_scene)
 
 func _on_ChangeAppearance_pressed():
 	get_tree().change_scene("res://src/scenes/ChangeAppearance.tscn")
@@ -20,18 +23,3 @@ func _on_Settings_pressed():
 	get_tree().change_scene("res://src/scenes/Settings.tscn")
 
 
-func _on_PearlButton1_pressed():
-	get_tree().change_scene("res://src/levels/Level1.tscn")
-	print("New Game pressed: going to level 1")
-
-
-func _on_PearlButton2_pressed():
-	get_tree().change_scene(nextScene)
-
-
-func _on_PearlButton3_pressed():
-	get_tree().change_scene("res://src/scenes/ChangeAppearance.tscn")
-
-
-func _on_tutorial_pressed():
-	get_tree().change_scene("res://src/components/how2play.tscn")
