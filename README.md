d28ff5f815716b190b2e4727189b2a7ca5536d74
josh <joshlagrimas1808@gmail.com>
2025-06-02 01:36:38 +0800

    delete

--- README.md
+++
@@ -1,1 +0,0 @@
-# Fish-Be-With-You
--- legacyFiles/NewGame.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	pass
-
-# connect to node tab
-#func _on_Button_pressed():
-#	get_tree().change_scene("res://src/levels/Level1.tscn")
-#	print("Hello World")
